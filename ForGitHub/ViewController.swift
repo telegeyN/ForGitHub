2fec7329a7f88957ee8a5b5c9314552355304340
telegeyN <ntelegey@gmail.com>
2024-02-08 00:49:51 +0600

    Initial Commit

---
+++ ForGitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForGitHub
+//
+//  Created by Telegey Nurbekova on 08/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
